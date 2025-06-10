3f1985bd663fbddb29c23d1f289e5d71b3b65de0
Shahdd011 <shahdelsayedd.11@gmail.com>
2025-06-10 11:29:42 +0300

    Update App.jsx

--- Craftopia/Frontend/craftopia-app/src/App.jsx
+++ Craftopia/Frontend/craftopia-app/src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from "react-router-dom";
 import { useState } from "react";
-
- // import WishlistProvider
+import { WishlistProvider } from "./context/WishlistContext";
+import { CartProvider } from "./context/CartContext";
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
@@ -13,42 +13,57 @@ import SignIn from "./Components/SignIn";
 import BestSellingProducts from "./Components/BestSellingProducts";
 import ArtistProfile from "./pages/ArtistProfile";
 import CustomerProfile from "./pages/CustomerProfile";
-import { WishlistProvider } from "./context/WishlistContext";
+import CartPage from "./pages/CartPage";
+
+function AppContent({ isLoggedIn, setIsLoggedIn }) {
+  const location = useLocation();
+  const hideNavbar = location.pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!hideNavbar && <Navbar isLoggedIn={isLoggedIn} />}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <WelcomeSection />
+              <PopularProducts />
+              <BestSellingProducts />
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/artist-profile" element={<ArtistProfile />} />
+        <Route path="/customer-profile" element={<CustomerProfile />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route
+          path="/login"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/landing" />
+            ) : (
+              <SignIn onLoginSuccess={() => setIsLoggedIn(true)} />
+            )
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <WishlistProvider>
-      <Router>
-        <Navbar isLoggedIn={isLoggedIn} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <WelcomeSection />
-                <PopularProducts />
-                <BestSellingProducts />
-                <Footer />
-              </div>
-            }
-          />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/landing" element={<LandingPage />} />
-          <Route path="/artist-profile" element={<ArtistProfile />} />
-          <Route path="/customer-profile" element={<CustomerProfile />} />
-          <Route
-            path="/login"
-            element={
-              isLoggedIn ? (
-                <Navigate to="/landing" />
-              ) : (
-                <SignIn onLoginSuccess={() => setIsLoggedIn(true)} />
-              )
-            }
-          />
-        </Routes>
-      </Router>
+      <CartProvider>
+        <Router>
+          <AppContent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        </Router>
+      </CartProvider>
     </WishlistProvider>
   );
 }
