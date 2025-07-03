8523745d8d3a7086b920d52b1ba7a42f60a88530
youssef abdellah <youssefabdellah10@outlook.com>
2025-07-03 16:15:18 +0300

    deploy the database done

--- Craftopia/Backend/src/config/db.js
+++ Craftopia/Backend/src/config/db.js
@@ -8,13 +8,13 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect:'postgres',
+        dialectOptions:{
+            ssl:{
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
         logging: false,
-        pool: {
-            max: 10,           
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
     }
 );
 
