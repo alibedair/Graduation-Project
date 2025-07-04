a04deb87cda6df6d02e90ae08d09b2271e76960c
youssef abdellah <youssefabdellah10@outlook.com>
2025-07-04 19:06:19 +0300

    add unit test

--- Craftopia/Backend/src/routes/messageRoute.js
+++ Craftopia/Backend/src/routes/messageRoute.js
@@ -44,28 +44,5 @@ router.patch('/read/:messageId',
     messageController.markMessageAsRead
 );
 
-router.get('/status/:userId',
-    authMiddleware,
-    roleMiddleware(['customer', 'artist']),
-    [
-        param('userId').isInt().withMessage('User ID must be an integer')
-    ],
-    messageController.getUserOnlineStatus
-);
-
-router.get('/conversation-status/:responseId',
-    authMiddleware,
-    roleMiddleware(['customer', 'artist']),
-    [
-        param('responseId').isInt().withMessage('Response ID must be an integer')
-    ],
-    messageController.getConversationStatus
-);
-
-router.get('/offline',
-    authMiddleware,
-    roleMiddleware(['customer', 'artist']),
-    messageController.getOfflineMessages
-);
 
 module.exports = router;
