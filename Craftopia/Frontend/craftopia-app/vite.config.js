ab5d762b41876baa1c0fa8945508ffa35c3497ba
youssef abdellah <youssefabdellah10@outlook.com>
2025-07-07 23:24:20 +0300

    Update vite.config.js

--- Craftopia/Frontend/craftopia-app/vite.config.js
+++ Craftopia/Frontend/craftopia-app/vite.config.js
@@ -7,11 +7,23 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '.openshiftapps.com',
+      'craftopia-frontend-youssefabdellah10-dev.apps.rm3.7wse.p1.openshiftapps.com'
+    ]
   },
   preview: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '.openshiftapps.com',
+      'craftopia-frontend-youssefabdellah10-dev.apps.rm3.7wse.p1.openshiftapps.com'
+    ]
   },
   base: '/'
 })
